24fee83338171635747ea4e17c942809d5b50e2c
Corentin Lemaitre <corentinlemaitre1996@gmail.com>
2018-03-17 18:50:38 +0100

    fix(deployement) : bug in script

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: false}))
 
 const router = express.Router()
 
-const staticFiles = express.static(path.join(__dirname, '../client/build'))
+const staticFiles = express.static(path.join(__dirname, '../../client/build'))
 app.use(staticFiles)
 
 router.get('/cities', (req, res) => {
